8647858eaba45b07003a64714a4f7d5e7ed6a543
stephy cyriac <cyriacstephy@gmail.com>
2021-10-09 09:21:17 +0530

    added razorpay and completed book my show

--- src/Pages/Movie.page.jsx
+++ src/Pages/Movie.page.jsx
@@ -1,6 +1,9 @@
 
 import {FaCcVisa , FaCcApplePay} from "react-icons/fa";
-
+import React,{useContext,useState,useEffect} from "react";
+import axios from "axios";
+import { useParams } from "react-router";
+import Slider from "react-slick";
 //config
 import TempPosters from "../config/TempPosters.config";
 
@@ -9,20 +12,58 @@ import MovieHero from "../components/MovieHero/MovieHero.component";
 import Cast from "../components/Cast/Cast.component";
 import PosterSlider from "../components/PosterSlider/PosterSlider.components";
 
+//context
+import { MovieContext } from "../contexs/movie.context";
+
+
+
  const MoviePage = () => {
+const {id} = useParams(); 
+const {movie} = useContext(MovieContext) ;
+const [cast,setCast] = useState([]);
+const [similarMovies,setSimilarMovies] = useState([]);
+const [recommended,setRecommended] = useState([]);
 
 
-    const settings = {
+useEffect(() => {
+
+    const requestCast = async() => {
+         const getCast = await axios.get(`/movie/${id}/credits`);
+         setCast(getCast.data.cast);
+    };
+    requestCast();
+} , [id]);
+
+
+useEffect(() => {
+    const requestSimilarMovies = async () => {
+  const getSimilarMovies = await axios.get(`/movie/${id}/similar`);
+  setSimilarMovies(getSimilarMovies.data.results); 
+    };
+  
+    requestSimilarMovies(); 
+  } , [id]);
+
+  useEffect(() => {
+    const requestRecommendedMovies = async () => {
+  const getRecommendedMovies = await axios.get(`/movie/${id}/recommendations`);
+  setRecommended(getRecommendedMovies.data.results); 
+    };
+  
+    requestRecommendedMovies(); 
+  } , [id]);
+
+    const settings = { 
         infinity:false,
         autoplay:false,
-        slidesToShow:3,
+        slidesToShow:4,
         slidesToScroll:2,
         InitialSlide: 0,
              responsive:[
                  {
                  breakpoints:1024,
                       settings:{
-                        slidesToShow:3,
+                        slidesToShow:4,
                         slidesToScroll:2,
                         infinite:true,
                                },
@@ -45,17 +86,47 @@ import PosterSlider from "../components/PosterSlider/PosterSlider.components";
                  },
                         ],
         };
-
+        const settingsCast = { 
+            infinity:false,
+            autoplay:false,
+            slidesToShow:6,
+            slidesToScroll:2,
+            InitialSlide: 0,
+                 responsive:[
+                     {
+                     breakpoints:1024,
+                          settings:{
+                            slidesToShow:4,
+                            slidesToScroll:2,
+                            infinite:true,
+                                   },
+                                   
+                     },
+                     {
+                        breakpoints:600,
+                        settings:{
+                          slidesToShow:4,
+                          slidesToScroll:2,
+                          initialSlide:1,
+                                 },
+                     },
+                     {
+                        breakpoints:480,
+                        settings:{
+                          slidesToShow:2,
+                          slidesToScroll:1,
+                                 },
+                     },
+                            ],
+            };
+    
     return (
         <>
            <MovieHero/>
 <div className="my-5  container  px-4 lg:ml-20 lg:w-2/3">
     <div className="flex flex-col items-start gap-3">
     <h2 className="text-gray-800 font-bold text-2xl">About the movie</h2>
-    <p>Bruce Wayne and Diana Prince try to bring the metahumans of Earth 
-        together after the death of Clark Kent. Meanwhile,
-         Darkseid sends Steppenwolf to Earth with an army to subjugate humans
-         </p>
+    <p>{movie.overview }</p>
     </div>
     
 <div className="my-8">
@@ -99,21 +170,14 @@ import PosterSlider from "../components/PosterSlider/PosterSlider.components";
 <hr/>
 </div>
 <h2 className="text-gray-800 font-bold text-2xl mb-4">Cast & Crew</h2>
-
-<div className="flex flex-wrap gap-6">
-    <Cast image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/ben-affleck-292-12-09-2017-05-12-16.jpg" 
-           castName="Ben Affleck" 
-           role="Batman"/>
-    <Cast image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/henry-cavill-23964-04-05-2020-04-25-14.jpg" 
-           castName="Henry Cavil" 
-           role="Superman"/>
-    <Cast image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/gal-gadot-11088-17-10-2017-11-45-36.jpg" 
-           castName="Gal Gadot" 
-           role="Wonder Woman"/>
-    <Cast image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/ray-fisher-1072729-17-10-2017-12-14-18.jpg" 
-           castName="Ray Fisher" 
-           role="Victor Stone/Cyborg"/>
-</div>
+    <Slider {...settingsCast}>
+    {cast.map((castdata) => (
+    <Cast image={`https://image.tmdb.org/t/p/original/${castdata.profile_path}`}
+           castName={castdata.original_name}
+           role={castdata.character}
+           />
+           ))}
+   </Slider>
 </div>
 <div className="my-8">
 <hr/>
@@ -121,7 +185,7 @@ import PosterSlider from "../components/PosterSlider/PosterSlider.components";
 <div className="my-8">
 <PosterSlider
      config = {settings}
-    images={TempPosters} 
+    images={similarMovies} 
     title="You might also like" 
     isDark={false}/>
 </div>
@@ -131,7 +195,7 @@ import PosterSlider from "../components/PosterSlider/PosterSlider.components";
 <div className="my-8">
 <PosterSlider
      config = {settings}
-    images={TempPosters} 
+    images={recommended} 
     title="BMS XCLUSIV" 
     isDark={false}/>
 </div>
--- src/components/MovieHero/MovieInfo.component.jsx
+++ src/components/MovieHero/MovieInfo.component.jsx
@@ -1,17 +1,34 @@
 //video wasn't uploaded
 
-import React,{useContext} from "react";
+import React,{useContext , useState} from "react";
+
+//components ----> payments
+import PaymentModal from "../PaymentModal/payment.component";
 
 //context
 import { MovieContext } from "../../contexs/movie.context";
 
+
 const MovieInfo = () => {
+  const [isOpen , setIsOpen] = useState(false);
+  const [price , setPrice] = useState(0);
   const {movie} = useContext(MovieContext);
 
   //optional changing
   const genres = movie.genres ?.map(({name})=> name).join(", ");
+
+  const rentMovies= () => {
+setIsOpen(true);
+setPrice(149);
+  };
+
+  const buyMovies= () => {
+    setIsOpen(true);
+    setPrice(599);
+      };
   return (
     <>
+    <PaymentModal setIsOpen={setIsOpen} isOpen={isOpen} price={price}/>
       <div className="flex flex-col gap-3 lg:gap-8">
         <div className="flex items-center gap-3 md:px-4">
           <div className="w-40 h-8">
@@ -34,11 +51,11 @@ const MovieInfo = () => {
             <h4>{(movie.runtime/60).toFixed(2)} h &bull; {genres} &bull; 13+</h4>
           </div>
           <div className="flex items-center gap-3 md:px-4 md:w-screen lg:w-full">
-            <button className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
+            <button onClick={rentMovies} className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
               Rent ₹149
             </button>
-            <button className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
-              Buy ₹149
+            <button onClick={buyMovies}  className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
+              Buy ₹599
             </button>
           </div>
         </div>
